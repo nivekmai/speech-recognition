386f3622d75f31a152cc4a1f6cf9e0fcc9a4aa0f
Kevin Christensen <nivekmai@gmail.com>
2019-08-18 14:14:45 -0700

    Updates

---
+++ precache-manifest.c06d71d539a689fbef213cae05eb72e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb2c1d09e2b225fef825",
+    "url": "/speech-recognition/static/js/runtime~main.cb2c1d09.js"
+  },
+  {
+    "revision": "1325742c49e2cd4c1f3d",
+    "url": "/speech-recognition/static/js/main.1325742c.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/speech-recognition/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "1325742c49e2cd4c1f3d",
+    "url": "/speech-recognition/static/css/main.3404f9b9.chunk.css"
+  },
+  {
+    "revision": "d8e1bed8dad94e9c18dcad28b71c925c",
+    "url": "/speech-recognition/index.html"
+  }
+];
